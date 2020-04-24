92d4938aa4a0c92c52481e0a5d89c6228b7305ac
MikhailGold <58542664+MikhailGold@users.noreply.github.com>
2020-04-24 19:20:20 +0600

    Add files via upload

---
+++ p_library/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render
+
+# Create your views here.
+from p_library.models import Book, Redaction
+from django.http.response import HttpResponse
+from django.template import loader
+from django.shortcuts import redirect
+
+
+def books_list(request):
+    books = Book.objects.all()
+    return HttpResponse(books)
+
+def index(request):
+    template = loader.get_template('index.html')
+    books = Book.objects.all()
+    biblio_data = {
+        "title": "мою библиотеку",
+        "books": books,
+    }
+    return HttpResponse(template.render(biblio_data, request))
+
+def book_increment(request):
+    if request.method == 'POST':
+        book_id = request.POST['id']
+        if not book_id:
+            return redirect('/index/')
+        else:
+            book = Book.objects.filter(id=book_id).first()
+            if not book:
+                return redirect('/index/')
+            book.copy_count += 1
+            book.save()
+        return redirect('/index/')
+    else:
+        return redirect('/index/')
+
+def book_decrement(request):
+    if request.method == 'POST':
+        book_id = request.POST['id']
+        if not book_id:
+            return redirect('/index')
+        else:
+            book = Book.objects.filter(id=book_id).first()
+            if not book:
+                return redirect('/index/')
+            if book.copy_count < 1:
+                book.copy_count = 0
+            else:
+                book.copy_count -= 1
+            book.save()
+        return redirect('/index/')
+    else:
+        return redirect('/index/')
+
+def redactions(request):
+    template = loader.get_template("redactions.html")
+    redactions = Redaction.objects.all()
+    data = {
+        "redactions": redactions,
+    }
+    return HttpResponse(template.render(data, request))
+
